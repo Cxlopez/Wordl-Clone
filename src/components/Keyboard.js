fb34b18383f2f907ddfd176c06eb5c1861cce405
Cxlopez <cristian4402@hotmail.com>
2022-12-04 20:52:14 -0500

    map keys2&3

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -11,8 +11,12 @@ const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
       <div className='line1'>{keys1.map((key) => {
         return <div> {key} </div>
       })}</div>
-      <div className='line2'></div>
-      <div className='line3'></div>
+      <div className='line2'>{keys2.map((key) => {
+        return <div> {key} </div>
+      })}</div>
+      <div className='line3'>{keys3.map((key) => {
+        return <div> {key} </div>
+      })}</div>
     </div>
   )
 }
