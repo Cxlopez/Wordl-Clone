3fda04fc0e4431ea22b6243374b938c6f7c1d5c2
Cxlopez <cristian4402@hotmail.com>
2022-12-08 18:56:08 -0500

    fix letterPos bug

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,47 +6,48 @@ function Board() {
 
   return (
     <div className='board'>
+      {" "}
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={0} />
-        <Letter letterPosition={1} attemptVal={0} />
-        <Letter letterPosition={2} attemptVal={0} />
-        <Letter letterPosition={3} attemptVal={0} />
-        <Letter letterPosition={4} attemptVal={0} />
+        <Letter letterPos={0} attemptVal={0} />
+        <Letter letterPos={1} attemptVal={0} />
+        <Letter letterPos={2} attemptVal={0} />
+        <Letter letterPos={3} attemptVal={0} />
+        <Letter letterPos={4} attemptVal={0} />
       </div>
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={1} />
-        <Letter letterPosition={1} attemptVal={1} />
-        <Letter letterPosition={2} attemptVal={1} />
-        <Letter letterPosition={3} attemptVal={1} />
-        <Letter letterPosition={4} attemptVal={1} />
+        <Letter letterPos={0} attemptVal={1} />
+        <Letter letterPos={1} attemptVal={1} />
+        <Letter letterPos={2} attemptVal={1} />
+        <Letter letterPos={3} attemptVal={1} />
+        <Letter letterPos={4} attemptVal={1} />
       </div>
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={2} />
-        <Letter letterPosition={1} attemptVal={2} />
-        <Letter letterPosition={2} attemptVal={2} />
-        <Letter letterPosition={3} attemptVal={2} />
-        <Letter letterPosition={4} attemptVal={2} />
+        <Letter letterPos={0} attemptVal={2} />
+        <Letter letterPos={1} attemptVal={2} />
+        <Letter letterPos={2} attemptVal={2} />
+        <Letter letterPos={3} attemptVal={2} />
+        <Letter letterPos={4} attemptVal={2} />
       </div>
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={3} />
-        <Letter letterPosition={1} attemptVal={3} />
-        <Letter letterPosition={2} attemptVal={3} />
-        <Letter letterPosition={3} attemptVal={3} />
-        <Letter letterPosition={4} attemptVal={3} />
+        <Letter letterPos={0} attemptVal={3} />
+        <Letter letterPos={1} attemptVal={3} />
+        <Letter letterPos={2} attemptVal={3} />
+        <Letter letterPos={3} attemptVal={3} />
+        <Letter letterPos={4} attemptVal={3} />
       </div>
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={4} />
-        <Letter letterPosition={1} attemptVal={4} />
-        <Letter letterPosition={2} attemptVal={4} />
-        <Letter letterPosition={3} attemptVal={4} />
-        <Letter letterPosition={4} attemptVal={4} />
+        <Letter letterPos={0} attemptVal={4} />
+        <Letter letterPos={1} attemptVal={4} />
+        <Letter letterPos={2} attemptVal={4} />
+        <Letter letterPos={3} attemptVal={4} />
+        <Letter letterPos={4} attemptVal={4} />
       </div>
       <div className='row'>
-        <Letter letterPosition={0} attemptVal={5} />
-        <Letter letterPosition={1} attemptVal={5} />
-        <Letter letterPosition={2} attemptVal={5} />
-        <Letter letterPosition={3} attemptVal={5} />
-        <Letter letterPosition={4} attemptVal={5} />
+        <Letter letterPos={0} attemptVal={5} />
+        <Letter letterPos={1} attemptVal={5} />
+        <Letter letterPos={2} attemptVal={5} />
+        <Letter letterPos={3} attemptVal={5} />
+        <Letter letterPos={4} attemptVal={5} />
       </div>
     </div>
   )
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-sequences */
-import React, {useContext} from 'react'
-import { AppContext } from '../App'
+import React, {useContext} from 'react';
+import { AppContext } from '../App';
 
-function Letter({ letterPosition, attemptVal }) {
+function Letter({ letterPos, attemptVal }) {
   const { board } = useContext(AppContext)
-  const letter = board[attemptVal, letterPosition];
-
-  return (
-    <div className='letter'>{letter}</div>
-  )
+  const letter = board[attemptVal][letterPos];
+  return <div className='letter'>{letter}</div>
 }
 
 export default Letter
