ea814fc7381d322b37244714f5d03bf83937de7d
Cxlopez <cristian4402@hotmail.com>
2022-12-08 19:08:01 -0500

    give ENTER btn conditions and function

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,15 +1,21 @@
-import React, { useContext } from 'react'
-import { AppContext } from "../App"
+import React, { useContext } from 'react';
+import { AppContext } from "../App";
 
-function Key({keyVal, bigKey}) {
-  const {board, setBoard, currentAttempt, setCurrentAttempt} = useContext(AppContext);
+function Key({ keyVal, bigKey }) {
+  const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext);
 
   const selectLetter = () => {
-    const newBoard = [...board];
-    newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
-    setBoard(newBoard);
-    setCurrentAttempt({attempt: currentAttempt.attempt, letterPos: currentAttempt.letterPos + 1});
-  }
+    if (keyVal === "ENTER") {
+      if (currentAttempt.letterPos !== 5) return;
+      setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0})
+    } else {
+      if (currentAttempt.letterPos > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+      setBoard(newBoard);
+      setCurrentAttempt({ attempt: currentAttempt.attempt, letterPos: currentAttempt.letterPos + 1 });
+    }
+  };
 
   return (
     <div className='key' id={bigKey && "big"} onClick={selectLetter} >{keyVal}</div>
