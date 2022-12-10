5a6b5a7a0ec8de0882cb93d0ebe9617586897919
Cxlopez <cristian4402@hotmail.com>
2022-12-09 21:15:06 -0500

    create onSelectLetter, onEnter, onDelete function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,29 @@ export const AppContext = createContext();
 function App() {
 
   const [board, setBoard] = useState(boardDefault)
-  const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPos: 0});
+  const [currentAttempt, setCurrentAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrentAttempt({ attempt: currentAttempt.attempt, letterPos: currentAttempt.letterPos + 1 });
+  }
+
+  const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) return;
+    setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0 })
+  }
+
+  const onDelete = () => {
+    if (currentAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrentAttempt({ ...currentAttempt, letterPos: currentAttempt.letterPos - 1 })
+  }
+
 
   return (
     <div className="App">
